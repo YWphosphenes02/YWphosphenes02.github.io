f2fc3b3c3aa995721088a2af4c41dfb8b23a16f9
YWphosphenes02 <wuyuzheng193@gmail.com>
2024-08-26 21:12:39 +0800

    Site updated: 2024-08-26 21:12:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/24/hello-world/","2024/08/24/编程笔记——C/"];function toRandomPost(){
+var posts=["2024/08/24/编程笔记——C/","2024/08/24/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
