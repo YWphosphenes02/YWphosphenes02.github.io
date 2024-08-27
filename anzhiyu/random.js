0db0fc2c27f0f9576c491ad898dd82643b43b766
YWphosphenes02 <wuyuzheng193@gmail.com>
2024-08-27 18:11:39 +0800

    Site updated: 2024-08-27 18:11:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/12/GitHub Pages托管启用方法/","2024/08/13/个人Blog框架介绍/","2024/08/02/假如在春天，一个旅人/","2024/07/25/编程笔记——C/"];function toRandomPost(){
+var posts=["2024/08/12/GitHub Pages托管启用方法/","2024/08/13/个人Blog框架介绍/","2024/07/25/编程笔记——C/","2024/08/02/假如在春天，一个旅人/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
